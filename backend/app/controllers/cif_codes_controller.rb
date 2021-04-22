ea2129eeaa3efed1cb535d98a7859cfb0717857c
daniel carvalho de oliveira <dco3@cin.ufpe.br>
2021-04-22 00:40:59 -0300

    added comments

--- backend/app/controllers/cif_codes_controller.rb
+++ backend/app/controllers/cif_codes_controller.rb
@@ -1,18 +1,26 @@
+##
+# This controller is for get cif_codes from database and render as json
+
 class CifCodesController < ApplicationController
+  ##
+  # Get all cif codes
   def index
     @codes = CifCode.all 
     render json: @codes, :except => :_id
   end
-
+  ##
+  # *Search for codes based on :description parameter*
   def search
     @codes = CifCode.where('$text' => {'$search' => cif_codes_params[:description].to_s}).order_by(code: :asc)
     render json: @codes, :except => :_id
   end
-
+  ##
+  # Get a list of codes based on its initials
   def show
     @codes = CifCode.where(initial: cif_codes_params[:initial]).order_by(code: :asc)
     render json: @codes, :except => :_id
   end
+
 
   private
   def cif_codes_params
--- backend/app/models/code_cif.rb
+++
@@ -1,6 +0,0 @@
-class CodeCif
-  include Mongoid::Document
-  field :code, type: String
-  field :type, type: String
-  field :description, type: String
-end
