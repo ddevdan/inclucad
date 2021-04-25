f68ea8d6864bf21414ab9bd430b4f6154932ed86
Daniel Carvalho <dco3@cin.ufpe.br>
2021-04-25 13:15:55 -0300

    [user] override devise token auth defaults

---
+++ backend/app/controllers/overrides/registrations_controller.rb
@@ -0,0 +1,70 @@
+module Overrides
+    class RegistrationsController < DeviseTokenAuth::RegistrationsController
+      before_action :set_user, only: [:update, :destroy]
+      def create
+          @resource            = resource_class.new(sign_up_params)
+          @resource.uid        = resource_params[:email]
+          @resource.provider   = "email"
+          binding.pry
+          begin
+            if @resource.save
+              render json: {
+                status: 'success',
+                data:   @resource.as_json
+              }
+            else
+              clean_up_passwords @resource
+              render json: {
+                status: 'error',
+                data:   @resource,
+                errors: @resource.errors
+              }, status: 403
+            end
+          rescue ActiveRecord::RecordNotUnique
+            clean_up_passwords @resource
+            render json: {
+              status: 'error',
+              data:   @resource,
+              errors: ["An account already exists for #{@resource.email}"]
+            }, status: 403
+          end
+        end
+
+        def update
+          if @resource
+            if @resource.send(resource_update_method, account_update_params.except(:hc_code))
+              yield @resource if block_given?
+              render_update_success
+            else
+              render_update_error
+            end
+          else
+            render_update_error_user_not_found
+          end
+        end
+
+        def destroy
+          
+          if @resource
+            @resource.destroy
+            yield @resource if block_given?
+            render_destroy_success
+          else
+            render_destroy_error
+          end
+        end
+
+
+        def set_user
+          @resource = User.find(account_update_params[:id])
+        end
+
+        def sign_up_params
+          params.require(:user).permit(*params_for_resource(:sign_up))
+        end
+
+        def account_update_params
+          params.require(:user).permit(*params_for_resource(:account_update))
+        end
+    end
+  end
