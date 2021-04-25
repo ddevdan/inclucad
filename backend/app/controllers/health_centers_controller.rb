446040392e585f9d44d0c092aec26f4374ca0935
Daniel Carvalho <dco3@cin.ufpe.br>
2021-04-25 13:53:26 -0300

    [health_center] before action authentication_user! added

--- backend/app/controllers/health_centers_controller.rb
+++ backend/app/controllers/health_centers_controller.rb
@@ -1,5 +1,6 @@
 class HealthCentersController < ApplicationController
   before_action :set_health_center, only: [:show, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /health_centers
   def index
