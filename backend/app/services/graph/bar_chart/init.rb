f0772e40cbf36821a9cc61e38af9cfd763069e1a
Daniel Carvalho <dco3@cin.ufpe.br>
2021-04-25 22:08:45 -0300

    [graph] describe code

--- backend/app/services/graph/bar_chart/init.rb
+++ backend/app/services/graph/bar_chart/init.rb
@@ -1,16 +1,19 @@
+##
+# Essa classe é utilizada para instanciar o gráfico de barra
 class Graph::BarChart::Init
     def self.structure
-        g = Graph.new
-        types = ['AUDITIVA', 'FÍSICA', 'INTELECTUAL', 'VISUAL', 
-        'OSTOMIA', 'MÚLTIPLAS DEFICIÊNCIAS']
-        cities = Address.where(address_type:"HealthCenter").distinct(:city)
-
+        ##
+        # city_hash armazena o resultado final, por cidade.
         city_hash = {}
+        ##
+        # Contabilizar os tipos de deficiencias de acordo com as avaliações
+        # que já foram feitas
         Evaluation.each do |ev|
             hc_city = to_upcase(ev.disabled_person.health_center.address.city)
             hc_state = to_upcase(ev.disabled_person.health_center.address.state)
             ev_type = to_upcase(ev.disabled_type)
-        
+            ##
+            # Criação da chaves e valores da cidade no city_hash
             if city_hash.has_key?(hc_city)
                 if city_hash[hc_city][:types].has_key?(ev_type)
                     city_hash[hc_city][:types][ev_type] +=1 unless ev_type.nil?
@@ -20,17 +23,25 @@ class Graph::BarChart::Init
             else
                 city_hash[hc_city] = {:state => hc_state, :types => {ev_type => 1} } unless ev_type.nil?
             end
-            
-            
-            
         end
-
+        ##
+        # Deletar gráficos gerados anteriormente para atualizar os dados
+        Graph.destroy_all
+        ##
+        # Gerar novos gráficos 
+        init_graphs(city_hash)
         
+    end
+
+    ##
+    # Gerar novos gráficos com os dados armazenados na variavel city_hash
+    def self.init_graphs(city_hash)
         city_hash.each do |city, val|
             data = []
             val[:types].each do |type, quantity|
                 data << {type: type, quantity: quantity }
             end
+            g = 
             g = Graph.create!(city: city, state: val[:state], 
                             type: "bar_chart", 
                             title:"Quantidade de Tipo de Deficiência por Cidade",
@@ -38,7 +49,7 @@ class Graph::BarChart::Init
         end
     end
 
-
+    # Tornar texto maiúsculo caso não seja nil
     def self.to_upcase(text)
         unless text.nil?
             return text.upcase
