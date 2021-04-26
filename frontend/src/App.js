a3c1d6493e7d0afcd12fe3491f204ef53a26368d
daniel carvalho de oliveira <dco3@cin.ufpe.br>
2021-04-26 20:06:40 -0300

    [frontend] first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        Oiiii
+      </div>
+    </div>
+  );
+}
+
+export default App;
