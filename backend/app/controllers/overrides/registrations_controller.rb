5226b313b5e0c2b73ad8ab6666ac8aaae2faea3b
Daniel Carvalho <dco3@cin.ufpe.br>
2021-04-26 01:41:11 -0300

    [user] removed custom configuration

--- backend/app/controllers/overrides/registrations_controller.rb
+++ backend/app/controllers/overrides/registrations_controller.rb
@@ -1,50 +1,7 @@
 module Overrides
     class RegistrationsController < DeviseTokenAuth::RegistrationsController
       before_action :set_user, only: [:update, :destroy]
-      before_action :authenticate_user!, only:[:destroy, :update]
-      def create
-          @resource            = resource_class.new(sign_up_params)
-          @resource.uid        = resource_params[:email]
-          @resource.provider   = "email"
-          binding.pry
-          begin
-            if @resource.save
-              render json: {
-                status: 'success',
-                data:   @resource.as_json
-              }
-            else
-              clean_up_passwords @resource
-              render json: {
-                status: 'error',
-                data:   @resource,
-                errors: @resource.errors
-              }, status: 403
-            end
-          rescue ActiveRecord::RecordNotUnique
-            clean_up_passwords @resource
-            render json: {
-              status: 'error',
-              data:   @resource,
-              errors: ["An account already exists for #{@resource.email}"]
-            }, status: 403
-          end
-        end
-
-        def update
-          if @resource
-            @health_center = HealthCenter.find(account_update_params[:health_center_attributes][:id])
-            @health_center.users << @resource unless @health_center.nil?
-            if @resource.send(resource_update_method, account_update_params.except(:hc_code))
-              yield @resource if block_given?
-              render_update_success
-            else
-              render_update_error
-            end
-          else
-            render_update_error_user_not_found
-          end
-        end
+      
 
         def destroy
           
