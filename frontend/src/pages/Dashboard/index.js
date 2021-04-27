3cd456bcd54af41d1c580aeec42be22027356c32
daniel carvalho de oliveira <dco3@cin.ufpe.br>
2021-04-27 03:37:33 -0300

    [dashboard] dashboard page created

---
+++ frontend/src/pages/Dashboard/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import DashButton from '../../components/DashButton'
+
+import * as s from './style'
+
+function Dashboard(props){
+    const {title} = props
+    return(
+        <s.Dashboard>
+            <s.Title>{title}</s.Title>
+            <s.WrapMenu>
+            <DashButton text="CADASTRAR" link_to="/create" img="create"/>
+            <DashButton text="PESQUISAR" link_to="/search" img="search"/>
+            <DashButton text="VISUALIZAR" link_to="/view" img="visualize"/>
+            <DashButton text="AVALIAR" link_to="/evaluation" img="evaluation"/>
+            </s.WrapMenu>
+        </s.Dashboard>
+    )
+}
+
+export default Dashboard
---
+++ frontend/src/style/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const App = styled.div`
+    background:#F2F2F6;
+    min-height: 100vh;
+    display:flex;
+    justify-content:center;
+    position:relative;
+
+`
+
