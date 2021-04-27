f2b90d226e738f64da88ad6a98e77cf10023c901
daniel carvalho de oliveira <dco3@cin.ufpe.br>
2021-04-27 03:39:43 -0300

    [react-router-dom] added react-router-dom to project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import './style/reset.css';
+import * as s from './style/style'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Menu from './components/Menu/';
+import Dashboard from './pages/Dashboard'
+import Evaluation from './pages/Evaluation';
 
 function App() {
   return (
-    <div className="App">
+    <s.App>
+    <Router>
       <div>
-        Oiiii
+        {/* <nav>
+          <ul>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            
+          </ul>
+        </nav> */}
+
+        <Switch>
+          <Route exact path="/">
+            <Dashboard title="Início"/>
+          </Route>  
+          <Route exact path="/evaluation">
+            <Evaluation title="Avaliações"/>
+          </Route>  
+
+        </Switch>
       </div>
-    </div>
+    <Menu/>
+
+    </Router>
+
+    </s.App>
   );
 }
 
