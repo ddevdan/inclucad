2e858e64d8e12263c89a7f07b221a95382adad0f
Daniel Carvalho de Oliveira <dco3@cin.ufpe.br>
2021-05-13 01:39:48 -0300

    [apiurl] change api url

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiBase = axios.create({
-  baseURL: "http://138.197.10.130:3000",
+  baseURL: "http://138.197.10.130:3000/",
   // baseURL:'https://3000-4711d7cb-ac39-427d-a0fc-555cc683f035.cs-us-east1-omte.cloudshell.dev'
   //prod url
 });
