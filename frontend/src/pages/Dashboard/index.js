e2e292d2c516b6abc78898442e0573d58462795e
Daniel Carvalho de Oliveira <dco3@cin.ufpe.br>
2021-05-15 02:07:03 -0300

    [renamed dashboard] renamed visualize page to dashboard

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,40 +1,68 @@
-import React, {useContext, useEffect, useState} from 'react'
-import DashButton from '../../components/DashButton'
-import AuthContext from '../../contexts/auth'
-import api from '../../api/api'
-import * as s from './style'
+import React, { useState, useEffect, useContext } from "react";
+import { useForm, Controller } from "react-hook-form";
+import { useHistory, useParams } from "react-router";
+import * as s from "./style";
+import TypeForm from "react-typeform";
+import api from "../../api/api";
+import Select from "react-select";
+import VerticalBar from "../../components/graphs/VerticalBar";
+import AuthContext from "../../contexts/auth";
 
-function Dashboard(props){
-    const context = useContext(AuthContext);
-    const {title, setLoggedIn = false} = props
-    const [agente, setAgente] = useState(false)
-    const [user, setUser] = useState(false)
-    useEffect(() => {
-        document.title = `INCLUCAD - Início`;
-        api.currentUser()
-        .then((res)=>{
-            console.log(res.data.data)
-            setLoggedIn({status:true, data:res.data.data})
-            setAgente(res.data.data.agente)
-            setUser(true)
-            
+function Dashboard(props) {
+  const { title } = props;
+  const [charts, setCharts] = useState({})
+  const [currentCity, setCurrentCity] = useState("paulista")
+  const [currentState, setCurrentState] = useState("recife")
+  const [loading, setLoading] = useState(true)
 
-        })
-        .catch(err=> console.log(err))
-      },[]);
-    return(
-        user ?
-        <s.Dashboard>
-            <s.Title>{title}</s.Title>
-            <s.WrapMenu>
-            {agente && <DashButton text="CADASTRAR" link_to="/register" img="create"/>}
-            <DashButton text="PESQUISAR" link_to="/search" img="search"/>
-            <DashButton text="VISUALIZAR" link_to="/view" img="visualize"/>
-            {!agente && <DashButton text="AVALIAR" link_to="/evaluations" img="evaluation"/> }
-            </s.WrapMenu>
-        </s.Dashboard>
-        : <></>
-    )
+  useEffect(() => {
+    document.title = `INCLUCAD - Dashboard`;
+    api.graph.barChart.get(currentCity)
+    .then((res)=>{
+      let charts_data = {}
+      const charts = res.data
+
+      charts.forEach(chart => {
+        switch (chart.type) {
+          case "bar_chart":
+            charts_data[chart.type] = chart.data[0]
+            break;
+        
+          default:
+            break;
+        }
+      }
+      );
+      setLoading(!loading)
+      setCharts(charts_data)
+
+
+
+    })
+    .catch(err=> console.log(err))
+
+    
+  },[currentCity]);
+
+  const changeCity = (e) =>{
+    const value = e.target.value
+    setCurrentCity(value)
+    setLoading(!loading)
+  }
+  return (
+  
+    <s.Dashboard>
+      <s.Title>Painel de Controle</s.Title>
+      <select onChange={changeCity} value={currentCity}>
+        <option value="paulista">Paulista</option>
+        <option value="recife">Recife</option>
+      </select>
+      {loading ? <p>Carrengando...</p> : <VerticalBar currentCity={currentCity} data={charts.bar_chart}/>}
+      
+    </s.Dashboard> 
+  )
 }
 
-export default Dashboard
+export default Dashboard;
+
+
